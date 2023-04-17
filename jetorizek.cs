0f45a6515aae5091cf4a5bc244d12c2e416d12ee
dav1duwu <100674187+dav1duwu@users.noreply.github.com>
2023-04-17 08:40:38 +0200

    Add files via upload

---
+++ jetorizek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvocislo
+{
+    public static partial class Matematika
+    {
+        private static string owo = "";
+        public static string prvo_cislo(int cislo)
+        {
+            bool prvocislo = false;            
+
+            if (cislo <= 2)
+            {
+                prvocislo = true;
+                return owo = prvocislo.ToString();
+            }           
+            else if (cislo % 2 == 0)
+            {
+                prvocislo = true;
+                return owo = prvocislo.ToString();                
+            }           
+            else
+            {
+                prvocislo = false;
+                return owo = prvocislo.ToString();
+            }
+        }
+    }
+}
