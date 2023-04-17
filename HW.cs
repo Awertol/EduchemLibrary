3f50fe4536a9e08f2cf024d9b938d78b4cd73979
Kohym <100674135+Kohym@users.noreply.github.com>
2023-04-17 08:18:10 +0200

    Add files via upload
    SEX 2.0

---
+++ HW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educhem
+{
+    public static partial class HW
+    {
+        public static double Pomer (double sirka, double vyska, double fin)
+        {
+            fin = sirka / vyska;
+            fin = Math.Round(fin, 2);
+            return fin;
+        }
+    }
+}
