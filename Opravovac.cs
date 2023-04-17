8bd2715daccd278c9f2e2a4cae5e9cc535862d3f
Kronos1904 <100674150+Kronos1904@users.noreply.github.com>
2023-04-17 08:43:57 +0200

    Add files via upload

---
+++ Opravovac.cs
@@ -0,0 +1,16 @@
+﻿namespace Educhem
+{
+    public class Predmet
+    {
+        public static partial class Cestina
+        {
+            public static string Opravovac(string a)
+            {
+                string first = a.Substring(0, 1);
+                a.Substring(0, 1).Replace(a.Substring(0, 1), first.ToUpper());
+                a += "!";
+                return a;
+            }
+        }
+    }
+}
