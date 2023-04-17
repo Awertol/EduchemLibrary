2c50bca63c2086563be8f43cf0441288b8a95d84
Joe Momma <100674130+joe-momm@users.noreply.github.com>
2023-04-17 08:38:30 +0200

    Add files via upload
    gregory

--- Matematika.cs
+++ Matematika.cs
@@ -1,14 +1,20 @@
-﻿namespace Educhem {
-    public static partial class Matematika {
-        /// <summary>
-        /// Zjistí jestli je trojúhelník pravoúhlý
-        /// </summary>
-        /// <returns>true jestli je trojúhelník pravoúhlý, false pokud není</returns>
-        public static bool JePravouhly(double triangleX, double triangleY, double triangleZ) {
-            double s = MathF.Asin((float)(triangleX / triangleZ));
-            double d = MathF.Asin((float)(triangleY / triangleZ));
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-            return Math.Floor((s + d) * 180 / Math.PI) == 90;
+namespace Educhem
+{
+    public static partial class Matematika
+    {
+        public static int priklad()
+        {
+            int polomer = 2;
+            int rNaDruhou = polomer * polomer;
+            int vyska = 5;
+            double vysledek = Math.PI * rNaDruhou * vyska;
+            return priklad();
         }
     }
 }
