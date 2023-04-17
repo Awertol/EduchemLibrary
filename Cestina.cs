e083723f4e0a37704828d32e185736e06760e393
zemanosek <100674053+zemanosek@users.noreply.github.com>
2023-04-17 08:36:15 +0200

    uz ne prosim

--- Cestina.cs
+++ Cestina.cs
@@ -1,15 +1,37 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System;
 
-namespace Educhem;
+namespace Educhem
+{
+    public static partial class Cestina
+    {
+        public static string ceskeUzemi(int stoleti)
+        {
+            Dictionary<int, string> Slovnik = new Dictionary <int, string>();
+            Slovnik.Add(1, "říše římská");
+            Slovnik.Add(2, "říše římská");
+            Slovnik.Add(3, "říše římská");
+            Slovnik.Add(4, "říše římská");
+            Slovnik.Add(5, "germánské kmeny");
+            Slovnik.Add(6, "Velkomoravská říše");
+            Slovnik.Add(7, "Velkomoravská říše");
+            Slovnik.Add(8, "Velkomoravská říše");
+            Slovnik.Add(9, "Velkomoravská říše");
+            Slovnik.Add(10, "knížectví české a knížectví moravské");
+            Slovnik.Add(11, "knížectví české a knížectví moravské");
+            Slovnik.Add(12, "Svatá říše římská");
+            Slovnik.Add(13, "Svatá říše římská");
+            Slovnik.Add(14, "Svatá říše římská");
+            Slovnik.Add(15, "Svatá říše římská");
+            Slovnik.Add(16, "Svatá říše římská");
+            Slovnik.Add(17, "Svatá říše římská");
+            Slovnik.Add(18, "Habsburská monarchie");
+            Slovnik.Add(19, "Rakouské císařství,");
+            Slovnik.Add(20, "Rakousko-Uhersko");
+            Slovnik.Add(21, "Česká Republika");
 
-public static partial class Cestina{
-    public static int NumberOfVowels(this string input){
-        string[] Vowels = new[]{
-            "A", "E", "I", "O", "U",
-            "Á", "É", "Í", "Ó", "Ú",
-            "Y", "Ý", "Ů"
-        };
-
-        return input.Count(x => Vowels.Any(y => y == x.ToString()));
+            string vysledek = Slovnik[stoleti];
+            return vysledek;
+        }
     }
 }
