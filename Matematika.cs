871553c87c099f17c5cb785f38092e3f0d95e1e3
J1rk0s <100620549+J1rk0s@users.noreply.github.com>
2023-04-17 08:12:20 +0200

    Add files via upload

---
+++ Matematika.cs
@@ -0,0 +1,10 @@
+﻿namespace Educhem {
+    public static partial class Matematika {
+        public static bool JePravouhly(double triangleX, double triangleY, double triangleZ) {
+            double s = MathF.Asin((float)(triangleX / triangleZ));
+            double d = MathF.Asin((float)(triangleY / triangleZ));
+
+            return Math.Floor((s + d) * 180 / Math.PI) == 90;
+        }
+    }
+}
