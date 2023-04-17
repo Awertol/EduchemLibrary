a521b5b109913b4f167752e69e14ff371c72e13e
B-L-A-K-E-1 <100674052+B-L-A-K-E-1@users.noreply.github.com>
2023-04-17 08:19:35 +0200

    Add files via upload
    barvičky

---
+++ RGBgenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Educhem{
+    public static partial class Grafika{
+
+            private static int MIN_COLOR_VALUE = 0;
+            private static int MAX_COLOR_VALUE = 255;
+            private static Random random;
+
+            public static Color RGBColorGenerator()
+            {
+                int red = random.Next(MIN_COLOR_VALUE, MAX_COLOR_VALUE + 1);
+                int green = random.Next(MIN_COLOR_VALUE, MAX_COLOR_VALUE + 1);
+                int blue = random.Next(MIN_COLOR_VALUE, MAX_COLOR_VALUE + 1);
+                return Color.FromArgb (red, green, blue);
+            }
+    }
+}
