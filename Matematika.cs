8128122f3e5b37fe99a2363ed2579b80741c634d
J1rk0s <100620549+J1rk0s@users.noreply.github.com>
2023-04-17 08:31:42 +0200

    Update Matematika.cs
    Popis metody

--- Matematika.cs
+++ Matematika.cs
@@ -1,5 +1,9 @@
 ﻿namespace Educhem {
     public static partial class Matematika {
+        /// <summary>
+        /// Zjistí jestli je trojúhelník pravoúhlý
+        /// </summary>
+        /// <returns>true jestli je trojúhelník pravoúhlý, false pokud není</returns>
         public static bool JePravouhly(double triangleX, double triangleY, double triangleZ) {
             double s = MathF.Asin((float)(triangleX / triangleZ));
             double d = MathF.Asin((float)(triangleY / triangleZ));
