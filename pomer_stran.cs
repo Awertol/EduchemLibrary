229fc4b6f984c664c64b677037d4f8b2edca87fe
OUGE-ge <84003050+OUGE-ge@users.noreply.github.com>
2023-04-17 08:51:48 +0200

    Add files via upload

---
+++ pomer_stran.cs
@@ -0,0 +1,22 @@
+﻿namespace Educhem
+{
+    public static partial class Hardware
+    {
+        public static string GetAspectRatio(int width, int height)
+        {
+            int gcd = GetGCD(width, height);
+            return $"{width / gcd}:{height / gcd}";
+        }
+
+        public static int GetGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
