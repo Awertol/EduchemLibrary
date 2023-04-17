f9f2ea1d15030bd8aa74685881279fd23020ec28
petrfehnl <101090891+petrfehnl@users.noreply.github.com>
2023-04-17 08:43:27 +0200

    Add files via upload
    chci umrit

---
+++ OdbornaPriprava.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace Educhem
+{
+ 
+
+    public static partial class OdbornaPriprava
+    {
+        
+        public static string OrganizaceKabelu(string nazevKonektoru)
+        {
+            string vysledek = "";
+            if (nazevKonektoru == "RJ11")
+                {
+                 vysledek = "KRBY";
+
+            }
+            else if (nazevKonektoru == "RJ12")
+            {
+                 vysledek = "UKRGYB";
+
+            }
+            else if (nazevKonektoru == "RJ45")
+            {
+                 vysledek = "NBOG";
+            }
+            return vysledek;
+        }
+        
+
+    }
+    
+}
