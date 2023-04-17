c37be09c8a609f8539549292a76cfb925663e690
TheDejvos <106534113+TheDejvos@users.noreply.github.com>
2023-04-17 08:34:37 +0200

    卐 卐 卐 卐 卐 卐 卐 卐 卐

---
+++ Cestina.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Educhem;
+
+public static partial class Cestina{
+    public static int NumberOfVowels(this string input){
+        string[] Vowels = new[]{
+            "A", "E", "I", "O", "U",
+            "Á", "É", "Í", "Ó", "Ú",
+            "Y", "Ý", "Ů"
+        };
+
+        return input.Count(x => Vowels.Any(y => y == x.ToString()));
+    }
+}
