1937b2ff7cfd389ca985a14f54ed931e2f556ca7
PukySbtw <100674127+PukySbtw@users.noreply.github.com>
2023-04-17 08:38:30 +0200

    Add files via upload

---
+++ Odbornapriprava.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educhem
+{
+    public static partial class Odbornapriprava
+    {
+        public static string chemZkratka(string chemZkratka)
+        {
+            var teploty = new Dictionary<string, string>() {
+            {"Au", "1 064 °C"},
+                {"Li","180,54 °C"},
+                {"Na","97,72 °C"}, 
+                {"K","63,38 °C"}
+            };
+            return teploty[chemZkratka];
+        }
+    }
+}
