17303ff9de0f12d9668071fe424235236a7b2d54
DonutMaste <100674075+DonutMaste@users.noreply.github.com>
2023-04-24 07:46:12 +0200

    Zjisti jestli jsi online

---
+++ OnlineStaff.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Educhem
+{
+    public static partial class Web
+    {
+        public static bool JeOnline()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send("192.168.112.77", 1000);
+                if (reply != null)
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+
+        }
+    }
+}
