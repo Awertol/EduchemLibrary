3413675eb037202a6790e7a87574afc576a52468
Michal Helgert <87770241+Awertol@users.noreply.github.com>
2023-04-24 08:04:38 +0200

    Update jetorizek.cs

--- jetorizek.cs
+++ jetorizek.cs
@@ -5,7 +5,7 @@ using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prvocislo
+namespace Educhem
 {
     public static partial class Matematika
     {
